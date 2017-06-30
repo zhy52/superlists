f1e060da68a435f4374ae230f7d1b7bd7b578df2
Zh Y2 <zhy521@outlook.com>
2017-06-30 23:32:21 +0800

    回滚后重新提交

--- lists/tests.py
+++ lists/tests.py
@@ -3,4 +3,4 @@ from django.test import TestCase
 # Create your tests here.
 class SmokeTest(TestCase):
     def test_bad_maths(self):
-        self.assertEqual(1+1,3)
+        self.assertEqual(1+1, 3)
