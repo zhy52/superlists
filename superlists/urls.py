bf3d6b9ea34ec191e9bc678bac3d49ad5a5445e8
SD-PC\Zh <zhy521@outlook.com>
2017-07-01 11:59:12 +0800

    本地仓库总是提示有未提交修改，尝试改正

--- superlists/urls.py
+++ superlists/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^$', 'lists.views.home', name='home')
-    url(r'^admin/', admin.site.urls),
+    # url(r'^admin/', admin.site.urls),
 ]
