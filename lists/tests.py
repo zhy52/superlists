ff3d1eb423301062f93731172bfd92b72ee874d2
SD-PC\Zh <zhy521@outlook.com>
2017-07-12 18:53:49 +0800

    Basic view now returns minimal HTML

--- lists/tests.py
+++ lists/tests.py
@@ -13,6 +13,6 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        self.assertTrue(reponse.content.startswith(b'<html>'))
+        self.assertTrue(response.content.startswith(b'<html>'))
         self.assertIn(b'<title>To-Do lists</title>', response.content)
         self.assertTrue(response.content.endswith(b'</html>'))
